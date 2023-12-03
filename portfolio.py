046326d4836c48b2481d86681c59a36c081e56cf
Gadomskyy <j_gadomski@hotmail.com>
2023-12-03 11:51:19 +0100

    Add addBalance and removeBalance methods

--- portfolio.py
+++ portfolio.py
@@ -1,5 +1,3 @@
-import stockinfo
-
 class Portfolio:
     def __init__(self):
         self.balance = 0
@@ -14,8 +12,13 @@ class Portfolio:
     def showPortfolio(self):
         pass
 
-    def addBalance(self):
-        pass
+    def addBalance(self, amount):
+        self.balance += amount
 
-    def removeBalance(self):
-        pass
+    def removeBalance(self, amount):
+        if amount > self.balance:
+            print("Amount to withdraw cannot be bigger than your balance.")
+        else:
+            self.balance = self.balance - amount
+        return self.balance
+
