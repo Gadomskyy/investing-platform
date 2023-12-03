c46f7c4041f191ad8922bd47d417786943e4c10c
Gadomskyy <j_gadomski@hotmail.com>
2023-12-03 11:43:18 +0100

    add file structure

---
+++ portfolio.py
@@ -0,0 +1,21 @@
+import stockinfo
+
+class Portfolio:
+    def __init__(self):
+        self.balance = 0
+        self.stocks = {}
+
+    def buyStock(self):
+        pass
+
+    def sellStock(self):
+        pass
+
+    def showPortfolio(self):
+        pass
+
+    def addBalance(self):
+        pass
+
+    def removeBalance(self):
+        pass
