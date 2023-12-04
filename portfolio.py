f7315807efcb248862b0b4e161e7002e25dc1b5d
Gadomskyy <118121980+Gadomskyy@users.noreply.github.com>
2023-12-04 19:21:53 +0100

    Create methods for Portfolio class (#3)
    * Add buyStock and showPortfolio methods
    
    * Add sellStock method
    
    * Add balance decrase to buyStocks
    
    * Add showBalance method

--- portfolio.py
+++ portfolio.py
@@ -1,16 +1,45 @@
+import stockinfo
+
+
 class Portfolio:
     def __init__(self):
         self.balance = 0
         self.stocks = {}
 
-    def buyStock(self):
-        pass
+    def showBalance(self):
+        print(f"Your balance is {self.balance}. ")
 
-    def sellStock(self):
-        pass
+    def buyStock(self, ticker, amount):
+        stock = stockinfo.stockInfo(stockinfo.api_key)
+        price = float(stock.getLatestStockPrice(ticker))
+        if price * amount > self.balance:
+            print("The stocks cost is bigger than your balance. Try again.")
+            return 0
+        if ticker in self.stocks:
+            self.stocks[ticker] += amount
+        else:
+            self.stocks[ticker] = amount
+        self.balance -= amount * price
+
+        return 0
+
+    def sellStock(self, ticker, amount):
+        stock = stockinfo.stockInfo(stockinfo.api_key)
+        price = float(stock.getLatestStockPrice(ticker))
+        if ticker not in self.stocks:
+            print("You do not have this stock in your portfolio.")
+            return 0
+        else:
+            if amount > self.stocks[ticker]:
+                print("You do not have a sufficient amount of stock. Please try again.")
+                return 0
+            else:
+                self.stocks[ticker] -= amount
+                self.balance += amount * price
+        return 0
 
     def showPortfolio(self):
-        pass
+        print("Your stock portfolio:", self.stocks)
 
     def addBalance(self, amount):
         if amount < 0:
@@ -26,3 +55,10 @@ class Portfolio:
             self.balance = self.balance - amount
         return self.balance
 
+x = Portfolio()
+x.addBalance(10000)
+x.buyStock("AAPL", 10)
+x.showPortfolio()
+x.sellStock("AAPL", 13)
+x.showPortfolio()
+x.showBalance()
