4aa15988fc1aa550a633627e5cda35c6958b4d69
Gadomskyy <j_gadomski@hotmail.com>
2023-12-12 23:01:36 +0100

    Change database from MySQL to MongoDB

--- database-connection.py
+++ database-connection.py
@@ -1,10 +1,25 @@
-import mysql.connector
+from pymongo import MongoClient
 
-mydb = mysql.connector.connect(host="localhost", user="root", passwd="qwerty123")
+class Database:
 
-mycursor = mydb.cursor()
+    def __init__(self):
+        self.client = MongoClient("mongodb://localhost:27017")
 
-def createDatabase(name):
-    mycursor.execute(f"Create database {name}")
+    def createDatabase(self, name):
+        self.db = self.client[name]
+
+    def createCollection(self, collection_name):
+        if hasattr(self, 'db'):
+            self.collection = self.db[collection_name]
+        else:
+            print("No database selected. Create database first.")
+
+x = Database()
+x.createDatabase("investingDB")
+x.createCollection("userinfo")
+mydict = { "name": "John", "id":  1}
+x.collection.insert_one(mydict)
+
+
 
 
