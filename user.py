2b814ef5f723af47d458637dde45b0fbd892e556
Gadomskyy <j_gadomski@hotmail.com>
2024-01-03 22:26:14 +0100

    Add checking password when logging in

--- user.py
+++ user.py
@@ -28,17 +28,24 @@ class User:
         if userRecord:
             print("Login successful!")
             # Set user attributes based database record
-            self.username = self.setAttribute(userRecord, "name")
-            self.password = self.setAttribute(userRecord, "password")
-            self.portfolio.stocks = self.setAttribute(userRecord, "stocks", {})
-            self.portfolio.balanceHistory = self.setAttribute(userRecord, "balancehistory", {})
-            self.portfolio.balance = self.setAttribute(userRecord, "balance", 0)
+            self.setUserRecords()
         else:
-            print("Invalid username or password. Please try again.")
+            if self.isLoginTaken(username):
+                self.checkPassword(username, password)
+                self.setUserRecords()
+            else:
+                print("No such login. Try logging again.")
 
-    def setAttribute(self, userRecord, field, failsafe=None):
+    @staticmethod
+    def setAttribute(userRecord, field, failsafe=None):
         return userRecord[field]
 
+    def setUserRecords(self):
+        self.username = self.setAttribute(self.userRecord, "name")
+        self.password = self.setAttribute(self.userRecord, "password")
+        self.portfolio.stocks = self.setAttribute(self.userRecord, "stocks", {})
+        self.portfolio.balanceHistory = self.setAttribute(self.userRecord, "balancehistory", {})
+        self.portfolio.balance = self.setAttribute(self.userRecord, "balance", 0)
 
     def setUsername(self):
         userChoice = input("Select a username for your account: ")
@@ -49,23 +56,26 @@ class User:
                userChoice = input("Username already taken. Please choose a different username: ")
         return userChoice
 
-    def setPassword(self):
+    @staticmethod
+    def setPassword():
         userChoice = input("Set a password for your account: ")
         while len(userChoice) < 6 or not(any(char.isdigit() for char in userChoice)):
             userChoice = input("Your password needs to be at least 6 characters long and contain at least one number. Try again. ")
         return userChoice
 
-    def checkPassword(self):
+    def checkPassword(self, username, userPassword):
         i = 3
-        userPassword = input("Please enter your password: ")
-        while(i > 1 and userPassword != self.password):
+        existing_username = self.database.collection.find_one({"name": username})
+        password = existing_username.get("password")
+        while(i > 1 and userPassword != password):
             i = i - 1
             userPassword = input(f"Incorrect password. Try again. You have {i} tries left. ")
-        if(userPassword != self.password):
+        if(userPassword != password):
             print("You didn't provide a correct password. Exiting program.")
             sys.exit()
         else:
-            print(f"Welcome, {self.username}!")
+            print(f"Welcome, {username}!")
+            return userPassword
 
     def isLoginTaken(self, username):
         existing = self.database.collection.find_one({"name": username})
