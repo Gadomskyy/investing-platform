1dcfe7eaf1282575bd328dd7712ef27e586b743d
Gadomskyy <118121980+Gadomskyy@users.noreply.github.com>
2024-01-03 22:47:59 +0100

    Login and Password Verification (#21)
    * Add check if login exists
    
    * Add checking password when logging in
    
    * Fix logging in when entering first wrong, then right credentials
    
    * Fix updating userRecord when using checkPassword
    
    * Remove redundant userRecord setting in checkPassword

--- user.py
+++ user.py
@@ -1,5 +1,4 @@
 import sys
-import time
 
 import dbase_conn
 from portfolio import Portfolio
@@ -23,44 +22,69 @@ class User:
         username = input("Enter your username: ")
         password = input("Enter your password: ")
 
-        userRecord = self.database.collection.find_one({"name": username, "password": password})
-        self.userRecord = userRecord
+        self.userRecord = self.findUser(username, password)
 
-        if userRecord:
+        if self.userRecord:
             print("Login successful!")
             # Set user attributes based database record
-            self.username = self.setAttribute(userRecord, "name")
-            self.password = self.setAttribute(userRecord, "password")
-            self.portfolio.stocks = self.setAttribute(userRecord, "stocks", {})
-            self.portfolio.balanceHistory = self.setAttribute(userRecord, "balancehistory", {})
-            self.portfolio.balance = self.setAttribute(userRecord, "balance", 0)
+            self.setUserRecords()
         else:
-            print("Invalid username or password. Please try again.")
+            if self.isLoginTaken(username):
+                correctPassword = self.checkPassword(username, password)
+                self.userRecord = self.findUser(username, correctPassword)
+                if self.userRecord:
+                    self.setUserRecords()
+            else:
+                print("No such login. Try logging again.")
 
-    def setAttribute(self, userRecord, field, failsafe=None):
+    @staticmethod
+    def setAttribute(userRecord, field, failsafe=None):
         return userRecord[field]
 
+    def findUser(self, username, password):
+        userRecord = self.database.collection.find_one({"name": username, "password": password})
+        return userRecord
+
+    def setUserRecords(self):
+        self.username = self.setAttribute(self.userRecord, "name")
+        self.password = self.setAttribute(self.userRecord, "password")
+        self.portfolio.stocks = self.setAttribute(self.userRecord, "stocks", {})
+        self.portfolio.balanceHistory = self.setAttribute(self.userRecord, "balancehistory", {})
+        self.portfolio.balance = self.setAttribute(self.userRecord, "balance", 0)
 
     def setUsername(self):
         userChoice = input("Select a username for your account: ")
-        while len(userChoice) < 3:
-            userChoice = input("Your username needs to be at least 3 characters long. Try again. ")
+        while len(userChoice) < 3 or self.isLoginTaken(userChoice):
+            if len(userChoice) < 3:
+                userChoice = input("Your username needs to be at least 3 characters long. Try again: ")
+            if self.isLoginTaken(userChoice):
+               userChoice = input("Username already taken. Please choose a different username: ")
         return userChoice
 
-    def setPassword(self):
+    @staticmethod
+    def setPassword():
         userChoice = input("Set a password for your account: ")
         while len(userChoice) < 6 or not(any(char.isdigit() for char in userChoice)):
             userChoice = input("Your password needs to be at least 6 characters long and contain at least one number. Try again. ")
         return userChoice
 
-    def checkPassword(self):
+    def checkPassword(self, username, userPassword):
         i = 3
-        userPassword = input("Please enter your password: ")
-        while(i > 1 and userPassword != self.password):
+        existing_username = self.database.collection.find_one({"name": username})
+        password = existing_username.get("password")
+        while(i > 1 and userPassword != password):
             i = i - 1
             userPassword = input(f"Incorrect password. Try again. You have {i} tries left. ")
-        if(userPassword != self.password):
+        if(userPassword != password):
             print("You didn't provide a correct password. Exiting program.")
             sys.exit()
         else:
-            print(f"Welcome, {self.username}!")
+            print(f"Welcome, {username}!")
+            return userPassword
+
+    def isLoginTaken(self, username):
+        existing = self.database.collection.find_one({"name": username})
+        if existing:
+            return True
+        return False
+
